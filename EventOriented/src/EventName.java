2578b34eac7d2ac83ee29c11f23fa5be434751ef
hdmonline <dongminhan@gatech.edu>
2019-03-23 16:49:16 -0400

    add code heads

--- EventOriented/src/EventName.java
+++ EventOriented/src/EventName.java
@@ -1,3 +1,11 @@
+/**
+ * EventName.java
+ * @author Group 41: Chong Ye, Dongmin Han, Shan Xiong
+ * Georgia Institute of Technology, Spring 2019
+ *
+ * The enum class of event names.
+ */
+
 public enum EventName {
     ArrivalSouth,
     Departure
--- EventOriented/src/Vehicle.java
+++ EventOriented/src/Vehicle.java
@@ -1,3 +1,11 @@
+/**
+ * Vehicle.java
+ * @author Group 41: Chong Ye, Dongmin Han, Shan Xiong
+ * Georgia Institute of Technology, Spring 2019
+ *
+ * The class of Vehicle.
+ */
+
 public class Vehicle {
     public int id;
     public double startTime;
