42dedcc54471b6fa65d4b272d3cdc1b01e16688d
chongye1991 <chongtt.ye@gmail.com>
2019-03-28 19:04:37 -0400

    updated EventOtiented

--- EventOriented/src/ProcessEvents.java
+++ EventOriented/src/ProcessEvents.java
@@ -6,19 +6,34 @@
  * The main entry point for CSE6730 Project 2
  */
 
+import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class ProcessEvents {
     // Event queue to store all the event in the order of time
-    static PriorityQueue<Event> eventQueue = new PriorityQueue<>();
+    private PriorityQueue<Event> eventQueue;
     // Current time
     private double now;
 
+    public ProcessEvents() {
+        this.now = 0;
+        this.eventQueue = new PriorityQueue<>();
+    }
+
     public static void main() {
+        ProcessEvents processor = new ProcessEvents();
+        EventHandler handler = EventHandler.getInstance();
+
+        // processing loop
+        Iterator itr = processor.eventQueue.iterator();
+        while (itr.hasNext()) {
+            Event currentEvent = processor.eventQueue.poll();
+            processor.now = currentEvent.time;
+            handler.handleEvent(currentEvent);
+        }
+
+
 
     }
 
-    private static void generateEventsFromInput() {
-
-    }
 }
--- EventOriented/src/Vehicle.java
+++ EventOriented/src/Vehicle.java
@@ -14,4 +14,14 @@ public class Vehicle {
     int entranceDirection;
     int exitIntersection;
     int exitDirection;
+
+    public Vehicle(int id, double startTime, double endTime, int entranceIntersection, int entranceDirection, int exitIntersection, int exitDirection) {
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.entranceIntersection = entranceIntersection;
+        this.entranceDirection = entranceDirection;
+        this.exitIntersection = exitIntersection;
+        this.exitDirection = exitDirection;
+    }
 }
